eff18f95a96c14c2c9051e28228f89abaaa9837a
sanjaydevang <sanjayvdevang067@gmail.com>
2025-05-13 21:15:09 -0400

    14th

---
+++ i/o.py
@@ -0,0 +1,22 @@
+name = input("enter your name : ")
+print(f"hello",name)
+
+
+
+
+name1 = []
+
+for i in range(5):
+    name1 = input("enter yoour name : ")
+    print(f"jello, {name1}")
+    
+
+
+    name2 = []
+    
+    for i in range(3):
+        name2.append(input("enter your name : "))
+        print(f"hello, {name2}")
+
+    for j in sorted(name2):
+        print(j)
---
+++ test1.py
@@ -0,0 +1,10 @@
+def main():
+    for i in range(1,10):
+        print(f"current number is {i}")
+        if i == 9:
+            print("its a blast brother")
+        else:
+            print("keep going")
+
+if __name__ == "__main__":
+    main()
